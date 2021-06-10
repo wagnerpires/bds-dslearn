c16582adc8f6aacc5bc44c3a336c1f489ee1cb6d
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-06-10 10:29:53 -0300

    Resource

--- backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -3,6 +3,8 @@ package com.devsuperior.dslearn.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_offer")
@@ -14,12 +16,18 @@ public class Offer implements Serializable {
     private Long id;
 
     private String edition;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant startMoment;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant endMoment;
 
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
+
+    @OneToMany(mappedBy = "offer")
+    private List<Resource> resources = new ArrayList<>();
 
     public Offer() {
 
@@ -71,6 +79,10 @@ public class Offer implements Serializable {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
     }
 
     @Override
