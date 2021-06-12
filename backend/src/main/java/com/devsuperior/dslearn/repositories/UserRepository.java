20438a6da66465f63ce7af57836b122067d41e14
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-06-11 21:11:51 -0300

    Exceptions, validation, security

--- backend/src/main/java/com/devsuperior/dslearn/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/UserRepository.java
@@ -4,5 +4,7 @@ import com.devsuperior.dslearn.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+    User findByEmail(String email);
 
 }
