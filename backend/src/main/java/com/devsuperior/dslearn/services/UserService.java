71cef6d02498c1581fe215dc3c066da7a2db58cb
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-06-11 21:26:13 -0300

    User by id

--- backend/src/main/java/com/devsuperior/dslearn/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearn/services/UserService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dslearn.services;
 
+import com.devsuperior.dslearn.dto.UserDTO;
+import com.devsuperior.dslearn.services.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslearn.entities.User;
 import com.devsuperior.dslearn.repositories.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -18,6 +23,13 @@ public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
